5ee25162009f553389b189156d3393c72ce436c6
Nikolay Ponomarev <itsnikolay@gmail.com>
2016-11-14 22:52:22 +0300

    dispatcher

--- app/assets/javascripts/dispatcher.js
+++ app/assets/javascripts/dispatcher.js
@@ -1,19 +1,33 @@
 window.onload = function () {
-  var input    = document.querySelector('#files');
-  var preview  = document.querySelector('#preview');
-  var download = document.querySelector('#download');
+  var body       = document.querySelector('body')
+  var dataset    = body.dataset;
+  var controller = dataset.controller;
+  var action     = dataset.action;
 
-  var fileInput = new FileInput(input);
-  var previewContainer = new PreviewContainer(preview);
-  var imageUploader = new ImageUploader();
-  var downloadLink = new DownloadLink(download);
+  switch (controller) {
+    case 'pdf_documents':
+      if (action === 'index') {
+        var input    = document.querySelector('#files');
+        var preview  = document.querySelector('#preview');
+        var download = document.querySelector('#download');
 
-  fileInput.on('readers:finished', imageUploader.upload.bind(imageUploader, fileInput));
-  imageUploader.on('ajax:success', fileInput.clearReaders.bind(fileInput));
-  imageUploader.on('ajax:success', downloadLink.refresh.bind(downloadLink));
-  imageUploader.on('ajax:success', function(response) {
-    var list = new PreviewImageList(response);
-    var elements = list.elements();
-    previewContainer.appendAll(elements);
-  });
+        var fileInput = new FileInput(input);
+        var previewContainer = new PreviewContainer(preview);
+        var imageUploader = new ImageUploader();
+        var downloadLink = new DownloadLink(download);
+
+        fileInput.on('readers:finished', imageUploader.upload.bind(imageUploader, fileInput));
+        imageUploader.on('ajax:success', fileInput.clearReaders.bind(fileInput));
+        imageUploader.on('ajax:success', downloadLink.refresh.bind(downloadLink));
+        imageUploader.on('ajax:success', function(response) {
+          var list = new PreviewImageList(response);
+          var elements = list.elements();
+          previewContainer.appendAll(elements);
+        });
+      }
+      break;
+
+    default:
+
+  }
 };
